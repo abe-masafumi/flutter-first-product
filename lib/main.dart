9eccd78ef31f20fe0c415ba77913105ce38be3bb
M1-Masa <masa455.wv@gmail.com>
2022-04-29 02:05:08 +0900

    add:画面UIクラスのみ別ページに移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/page1.dart';
 // ignore: unused_import
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -30,36 +31,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// 余計なコードを削除してfirebase_storeとコネクト
-class MyHomePage extends StatelessWidget {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('pets-info-01').snapshots();
-  @override
-    Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // headerのtitleの指定
-        title: Text("firebase-data一覧"),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return ListTile(
-              title: Text(data['名前']),
-              subtitle: Text(data['誕生日']),
-            );
-          }).toList(),
-        );
-      },
-     ),
-    );
-  }
-}
+// ※※※別ページに移動※※※
+// // 余計なコードを削除してfirebase_storeとコネクト
+// class MyHomePage extends StatelessWidget {
+//   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('pets-info-01').snapshots();
+//   @override
+//     Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // headerのtitleの指定
+//         title: Text("firebase-data一覧"),
+//       ),
+//       body: StreamBuilder<QuerySnapshot>(
+//       stream: _usersStream,
+//       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+//         if (snapshot.hasError) {
+//           return Text('Something went wrong');
+//         }
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return Text("Loading");
+//         }
+//         return ListView(
+//           children: snapshot.data!.docs.map((DocumentSnapshot document) {
+//           Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+//             return ListTile(
+//               title: Text(data['名前']),
+//               subtitle: Text(data['誕生日']),
+//             );
+//           }).toList(),
+//         );
+//       },
+//      ),
+//     );
+//   }
+// }
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,41 +1,36 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:flutter_application_1/main.dart';
+// 余計なコードを削除してfirebase_storeとコネクト
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-// class _MyHomePageState extends StatelessWidget<MyHomePage> {
-//   // firestoreのdocから追記して"FirebaseFirestore"のimport
-//   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('pets-info-01').snapshots();
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         // headerのtitleの指定
-//         title: Text(widget.title),
-//       ),
-//       body: StreamBuilder<QuerySnapshot>(
-//       stream: _usersStream,
-//       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//         if (snapshot.hasError) {
-//           return Text('Something went wrong');
-//         }
-
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Text("Loading");
-//         }
-
-//         return ListView(
-//           children: snapshot.data!.docs.map((DocumentSnapshot document) {
-//           Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-//             return ListTile(
-//               title: Text(data['名前']),
-//               subtitle: Text(data['誕生日']),
-//             );
-//           }).toList(),
-//         );
-//       },
-//      ),
-//     );
-//   }
-// }
+class MyHomePage extends StatelessWidget {
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('pets-info-01').snapshots();
+  @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // headerのtitleの指定
+        title: Text("firebase-data一覧"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['名前']),
+              subtitle: Text(data['誕生日']),
+            );
+          }).toList(),
+        );
+      },
+     ),
+    );
+  }
+}
